91f9ad2f4774461eb7bb1dbfdd378040283b747e
jinhyuck8504 <jinhyucks@gmail.com>
2025-08-04 12:05:17 +0900

    Update SecureInput.tsx

--- src/components/security/SecureInput.tsx
+++ src/components/security/SecureInput.tsx
@@ -204,7 +204,7 @@ export default function SecureInput({
           onBlur={() => setIsFocused(false)}
           className={`pr-10 ${
             validationResult.isValid ? 'border-gray-300' : 'border-red-300'
-          } ${props.className || ''}`}
+          }`}
           maxLength={maxLength}
         />
         
@@ -318,7 +318,7 @@ export function SecureTextarea({
         onChange={handleChange}
         className={`w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 ${
           validationResult.isValid ? 'border-gray-300' : 'border-red-300'
-        } ${props.className || ''}`}
+        }`}
         maxLength={maxLength}
       />
 
